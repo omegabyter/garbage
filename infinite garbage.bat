a8d85f4833159d474332f47227ae36d35f51606b
omegabyter <111038163+omegabyter@users.noreply.github.com>
2022-11-29 01:57:35 -0500

    garbage uploading

---
+++ infinite garbage.bat
@@ -0,0 +1,54 @@
+@echo off
+title begin the garbage forever
+echo press any key to begin the garbage.. forever. you may exit at any time
+pause >nul
+echo tip: hold space to speed up the garbage
+
+:garbage
+title garbage
+echo garbage
+color 01
+timeout 1 >nul
+echo garbage
+color 02
+timeout 1 >nul
+echo garbage
+color 03
+timeout 1 >nul
+echo garbage
+color 04
+timeout 1 >nul
+echo garbage
+color 05
+timeout 1 >nul
+echo garbage
+color 06
+timeout 1 >nul
+echo garbage
+color 07
+timeout 1 >nul
+echo garbage
+color 08
+timeout 1 >nul
+echo garbage
+color 09
+timeout 1 >nul
+echo garbage
+color 0A
+timeout 1 >nul
+echo garbage
+color 0B
+timeout 1 >nul
+echo garbage
+color 0C
+timeout 1 >nul
+echo garbage
+color 0D
+timeout 1 >nul
+echo garbage
+color 0E
+timeout 1 >nul
+echo garbage
+color 0F
+timeout 1 >nul
+goto garbage
